750765ca3e06726c8a0fdbb741be46c3ce47394b
samy khelifi <Samy.Khelifi@ign.fr>
2020-11-17 17:32:35 +0100

    [DELETE] unused import in datasets.py

--- odeon/nn/datasets.py
+++ odeon/nn/datasets.py
@@ -1,8 +1,6 @@
-import os
 from torch.utils.data import Dataset
-from skimage.util import img_as_float, img_as_ubyte
+from skimage.util import img_as_float
 import rasterio
-from rasterio.plot import reshape_as_raster
 # from rasterio.plot import reshape_as_raster
 import numpy as np
 from odeon.commons.image import image_to_ndarray, raster_to_ndarray, CollectionDatasetReader
