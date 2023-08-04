1e43d8452447378ce97b96c413f8b48f205a3f6d
samy <samy.khelifi@ign.fr>
2023-08-04 10:20:53 +0200

    [WIP] stach commit and mypy error fix

--- odeon/core/tile.py
+++ odeon/core/tile.py
@@ -12,7 +12,7 @@ from .vector import box, create_box_from_bounds
 logger = logging.getLogger(__name__)
 
 
-def tile(bounds: List, tile_size: GeoTuple = 256.0, overlap: GeoTuple = 0,
+def tile(bounds: List, tile_size: int | float | None | GeoTuple = 256.0, overlap: int | float | None | GeoTuple = 0,
          strict_inclusion: bool = True, fit_extent: bool = False) -> Generator[Dict, None, None]:
     """
     Simple function to tile with a regular step in X-axis and Y-axis
