81fbe0206e1237d40b77e89e17bc3c113eaa80c1
samy <samy.khelifi@ign.fr>
2023-12-03 12:24:41 +0100

    [WIP] multi task change unet

--- odeon/models/segmentation/segmentation.py
+++ odeon/models/segmentation/segmentation.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from pytorch_lightning import LightningModule
 from pytorch_lightning.utilities.types import STEP_OUTPUT
 from segmentation_models_pytorch import create_model
-
+from segmentation_models_pytorch.losses import FocalLoss, TverskyLoss
 from odeon.models.core.models import MODEL_REGISTRY
 
 __all__ = ['SegmentationModule']
@@ -13,7 +13,9 @@ __all__ = ['SegmentationModule']
 
 @MODEL_REGISTRY.register(name='segmentation_model', aliases=['seg_model', 'seg_mod'])
 class SegmentationModule(LightningModule):
+    """Segmentation module for multiclass segmentation with pytorch segmentation models
 
+    """
     def __init__(self,
                  classes: int,
                  model_name: str,
@@ -21,16 +23,19 @@ class SegmentationModule(LightningModule):
                  encoder_name: str = "resnet34",
                  encoder_weights: Optional[str] = None,
                  in_channels: int = 3,
-                 **kwargs,
+                 model: Optional[nn.Module] = None,
+                 **kwargs
                  ):
 
         super(SegmentationModule, self).__init__()
+
         self.model = create_model(arch=model_name,
                                   encoder_name=encoder_name,
                                   encoder_weights=encoder_weights,
                                   in_channels=in_channels,
                                   classes=classes,
-                                  **kwargs)
+                                  **kwargs) if model is None else model
+
         self.criterion = loss(reduction='mean')
 
     def training_step(self, sample, *args, **kwargs) -> STEP_OUTPUT:
