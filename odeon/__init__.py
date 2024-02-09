1df0c0086fa4ba4cab219b40e0cdf22055d1cd6f
samy <samy.khelifi@ign.fr>
2024-02-09 12:23:17 +0100

    [WIP] intermediate commit

--- odeon/__init__.py
+++ odeon/__init__.py
@@ -3,24 +3,47 @@
 import pathlib
 from pathlib import Path
 
+from omegaconf import OmegaConf
+
 from .core.python_env import debug_mode
 from .core.logger import get_logger
 from .core.env import get_env, Env
-
 from .core.default_path import ODEON_ENV, ODEON_PATH
+
+__name__ = 'odeon'
+
+# DEFAULT_ODEON_PATH: Path = HOME
+_this_dir: Path = pathlib.Path(__file__).resolve().parent
+with (_this_dir / ".." / "VERSION").open() as vf:
+    __version__ = vf.read().strip()
+
 
 logger = get_logger(__name__, debug=debug_mode)
 ENV: Env = get_env()
 ENV.load_plugins()  # load the configured plugins
 logger.debug(f'env plugins: {ENV.plugins}')
 
+
+def store_resolver(store_name: str) -> str:
+    match store_name:
+        case 'user_config_store':
+            return str(ENV.user_config_store)
+        case 'user_artefact_store':
+            return str(ENV.user_artefact_store)
+        case 'user_model_store':
+            return str(ENV.user_model_store)
+        case 'user_dataset_store':
+            return str(ENV.user_dataset_store)
+        case 'user_test_store':
+            return str(ENV.user_test_store)
+        case 'user_delivery_store':
+            return str(ENV.user_delivery_store)
+        case 'user_log_store':
+            return str(ENV.user_log_store)
+        case _:
+            raise ValueError(f'the store {store_name} is not a valid store name')
+
+
+OmegaConf.register_new_resolver(name='odn.store', resolver=store_resolver)
+
 __all__ = ['ENV', 'ODEON_ENV', 'ODEON_PATH']
-
-# from .fit introspection.py FitApp, fit_plugin, pl_callback_plugin, pl_logger_plugin
-
-
-# DEFAULT_ODEON_PATH: Path = HOME
-_this_dir: Path = pathlib.Path(__file__).resolve().parent
-
-with (_this_dir / ".." / "VERSION").open() as vf:
-    __version__ = vf.read().strip()
--- odeon/core/python_env.py
+++ odeon/core/python_env.py
@@ -1,16 +1,40 @@
-def is_running_in_jupyter_notebook():
+""" Python Environment for Odeon module"""
+# create a _python_env.py module for these functions and that on just for the exposed constants
+
+
+def is_running_from_ipython():
     try:
         from IPython import get_ipython
+        return get_ipython() is not None
+    except ImportError:
+        return False
+    except AttributeError:
+        return False
+
+
+def is_running_in_jupyter_notebook():
+    # TODO simplify code between functions is_running_in_jupyter_notebook and is_running_from_ipython
+
+    if is_running_from_ipython() is False:
+        return False
+    try:
+        from IPython import get_ipython
+    except ImportError:
+        return False
+    except AttributeError:
+        return False
+    if get_ipython() is not None:
         if 'IPKernelApp' not in get_ipython().config:
             return False
-    except Exception:
+        else:
+            return True
+    else:
         return False
-    return True
 
 
 def debug_mode() -> bool:
     from os import environ
-    return str(environ.get('ODEON_DEBUG_MODE')) == '1'
+    return str(environ.get('ODEON_DEBUG')) == '1'
 
 
 debug_mode = debug_mode()
