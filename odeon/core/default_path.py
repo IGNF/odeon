eaa8953bc7b5bdad57141597334a9595cfb8a76e
samy KHELIFI <samy.khelifi.richards@gmail.com>
2024-04-03 16:54:35 +0200

    Foret to dev, add doc to data internal plugin (#12)
    * data: handling csv with no header and one hot encoding mask
    
    * fixes for forrest mask use case
    
    * FIX mypy error on data module and fit app
    
    * resolve mypy issue and add non user stores to custom resolver
    
    * v0.5.0
    
    * v0.5.0 fix mypy error core/init
    
    * v0.5.0 fix changelog version error
    
    * fix mypy error data module
    
    * few linting
    
    * document internal data plugin

--- odeon/core/default_path.py
+++ odeon/core/default_path.py
@@ -6,5 +6,5 @@ INSTALL_PATH_VAR: str | None = os.environ.get("ODEON_INSTALL_PATH")
 if INSTALL_PATH_VAR:
     ODEON_PATH: Path = Path(INSTALL_PATH_VAR, '.odeon')
 else:
-    ODEON_PATH: Path = Path(HOME, '.odeon')
+    ODEON_PATH = Path(HOME, '.odeon')
 ODEON_ENV: Path = Path(ODEON_PATH, 'env.yaml')
