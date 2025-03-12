7ee2550ff798e259e27506b1182bcfbfc224edc5
samy KHELIFI <samy.khelifi.richards@gmail.com>
2025-03-12 16:44:12 +0100

    update requirements in setup.py, use ruff instead of black, flake8, and isort

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ logger = getLogger(__name__)
 try:
     with open(str(Path(__file__).parent / "README.md"), "r") as fh:
         long_description = fh.read()
-except FileNotFoundError as e:
+except FileNotFoundError:
     logger.error("Could not find the README.md file during setup install")
 try:
     with open(str(Path(__file__).parent / "VERSION"), "r") as fh:
@@ -16,43 +16,61 @@ try:
 except FileNotFoundError:
     logger.error("Could not find the VERSION file during setup install")
 setup(
-    name='odeon',
+    name="odeon",
     version=version,  # Replace with your desired version or load from file as before
-    author='samy khelifi',
-    author_email='samy.khelifi@ign.fr',
-    description='odeon is a framework for development of machine learning pipeline in remote sensing',
+    author="samy khelifi",
+    author_email="samy.khelifi@ign.fr",
+    description="odeon is a framework for development of machine learning pipeline in remote sensing",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/IGNF/odeon',
-    project_urls={
-        'Bug Tracker': 'https://github.com/IGNF/odeon/issues'
-    },
+    long_description_content_type="text/markdown",
+    url="https://github.com/IGNF/odeon",
+    project_urls={"Bug Tracker": "https://github.com/IGNF/odeon/issues"},
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Scientific/Engineering :: Image Recognition'
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
     ],
-    packages=find_packages('.'),
-    python_requires='>=3.10',
+    packages=find_packages("."),
+    python_requires=">=3.10",
     install_requires=[
-        'omegaconf',
-        'jsonargparse',
-        'pandas',
-        'geopandas>=0.10',
-        'rasterio>=1.1.5',
-        'torch>=1.6',
-        'pytorch-lightning>=2.1'
+        "tqdm",
+        "numpy>=1.17.2",
+        "omegaconf>=2.1",
+        "jsonargparse",
+        # Geo Dependencies
+        "geopandas>=0.10",
+        "fiona>=1.8",
+        "pyproj>=2.2",
+        "rasterio>=1.0.20",
+        "rtree>=1",
+        "shapely>=1.3",
+        "pyogrio",
+        # Machine Learning / Dataviz / Computer Vision
+        "matplotlib",
+        "scikit-learn>=0.21",
+        "seaborn",
+        "matplotlib-base>=3.3,<4",
+        "pillow",
+        "opencv-python-headless>=4.7.0",
+        # Deep Learning / Pytorch ecosystem
+        "torch>=2.0.0",
+        "torchvision",
+        "pytorch-lightning",
+        "segmentation-models-pytorch",
+        "timm",
+        "torchmetrics",
+        "tensorboard",
+        "tensorboardx",
+        "albumentations",
+        "kornia>=0.7",
+        "einops",
     ],
-    entry_points={
-        'console_scripts': [
-            'odeon = odeon.main:main'
-        ]
-    },
-    package_data={'odeon': ['../VERSION', '../README.md', '../LICENSE', '../README_fr-FR.md']},
+    entry_points={"console_scripts": ["odeon = odeon.main:main"]},
+    package_data={"odeon": ["../VERSION", "../README.md", "../LICENSE", "../README_fr-FR.md"]},
     include_package_data=True,
 )
